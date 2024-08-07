10f5d31ad6d604ef6730615786a53928b358d6e5
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-08-07 13:11:29 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
 
     <div>
-      <h1 className="text-red-300 mb-5 bg-green-500 py-10">hello world</h1>
+      <Button>Teste</Button>
     </div>
   );
 }
