e667a1f54a35c449621444af1d8c0c47974ed5ee
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-08-07 13:53:15 -0300

    chore: add git commit msg linter setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "./_components/ui/button"
 export default function Home() {
   return (
     <div>
-      <Button>só de teste</Button>{" "}
+      <Button>só de teste</Button>
     </div>
   )
 }
