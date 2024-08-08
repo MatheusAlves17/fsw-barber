bbd6a2dce69e2d19532720caaa113df3140ecc20
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-08-08 11:03:34 -0300

    feat: add badge

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { Input } from "./_components/ui/input"
 import Image from "next/image"
 
 import Banner from "@/public/banner-01.png"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -29,6 +32,27 @@ export default function Home() {
             className="rounded-xl"
           />
         </div>
+        <Card className="mt-6">
+          <CardContent className="p-0">
+            <div className="flex justify-between">
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit">Agendamento</Badge>
+                <h3 className="font-semibold">Corte de cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png" />
+                  </Avatar>
+                  <p className="text-sm">Barbearia FSW</p>
+                </div>
+              </div>
+              <div className="flex flex-col items-center justify-center border-l-2 border-solid border-white px-5">
+                <p className="text-sm">Fevereiro</p>
+                <p className="text-2xl">06</p>
+                <p className="text-sm">09:45</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
