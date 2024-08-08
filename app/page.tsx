ee09d1a46dc5e63ea9dabfa51b13ce50c5f5a2b2
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-08-08 10:38:34 -0300

    feat: banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+import { Search } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+
+import Banner from "@/public/banner-01.png"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Button>só de teste</Button>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Matheus!</h2>
+        <p>Quarta-feira, 07 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button size="icon">
+            <Search />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src={Banner}
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            objectFit="cover"
+            className="rounded-xl"
+          />
+        </div>
+      </div>
     </div>
   )
 }
