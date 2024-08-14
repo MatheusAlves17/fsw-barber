7eea0f214faf7a5f7ab1b8454774277578e49a60
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-08-14 11:00:39 -0300

    chore: add setup next-auth

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,10 +1,19 @@
-import { Calendar, HomeIcon, LogOutIcon } from "lucide-react"
+import { Calendar, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearch } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+import Google from "@/public/google.svg"
 
 function SideBar() {
   return (
@@ -12,7 +21,34 @@ function SideBar() {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="flex gap-3 border-b border-solid py-5">
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu Login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-11/12 rounded-lg">
+            <DialogHeader>
+              <DialogTitle>Faça seu Login!</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant={"outline"} className="gap-2 font-bold">
+              <Image
+                alt="Faça login usando sua conta Google"
+                src={Google}
+                width={20}
+                height={20}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+      {/* <div className="flex gap-3 border-b border-solid py-5">
         <Avatar>
           <AvatarImage
             src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -23,7 +59,7 @@ function SideBar() {
           <p className="font-bold">Mario Arnaldo</p>
           <p className="text-xs">mario@atras.com</p>
         </div>
-      </div>
+      </div> */}
       <div className="flex flex-col gap-4 border-b border-solid py-5">
         <SheetClose asChild>
           <Button className="justify-start gap-2" asChild>
